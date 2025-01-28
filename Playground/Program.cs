e49d01b5b4a7dc11fbcf9ddca67d9e06ddc3fa77
ricky <ricky.y.c@outlook.com>
2025-01-28 14:07:43 +0800

    add playground

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,2 +1,34 @@
-﻿
+﻿using GeneralAlgorithms.DataStructures.Polygons;
+using MapGeneration.Core.Doors.DoorModes;
+using MapGeneration.Core.MapDescriptions;
+using MapGeneration.Interfaces.Core.Doors;
+using MapGeneration.Utils;
 
+var mapDescription = new MapDescription<int>();
+// Add rooms ( - you would normally use a for cycle)
+mapDescription.AddRoom(0);
+mapDescription.AddRoom(1);
+mapDescription.AddRoom(2);
+mapDescription.AddRoom(3);
+// Add passages
+mapDescription.AddPassage(0, 1);
+mapDescription.AddPassage(0, 3);
+mapDescription.AddPassage(1, 2);
+mapDescription.AddPassage(2, 3);
+
+var doorMode = new OverlapMode(1, 1);
+
+var squareRoom = new RoomDescription(
+  GridPolygon.GetSquare(8),
+  doorMode
+);
+var rectangleRoom = new RoomDescription(
+  GridPolygon.GetRectangle(6, 10),
+  doorMode
+);
+mapDescription.AddRoomShapes([squareRoom, rectangleRoom]);
+
+var generator = LayoutGeneratorFactory.GetDefaultChainBasedGenerator<int>();
+var layout = generator.GetLayouts(mapDescription, 1)[0];
+
+_ = Console.ReadKey();
